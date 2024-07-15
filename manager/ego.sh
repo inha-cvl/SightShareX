b6967020e3a11d701286de6cad5576d422eb0d70
inha-cvl <cvlab.inhauniv@gmail.com>
2024-07-15 17:01:52 +0900

    [test] Ioniq5

--- manager/ego.sh
+++ manager/ego.sh
@@ -3,7 +3,7 @@
 cd ../sharing_info
 python3 main.py ego Solbat &
 cd ../v2x
-python3 main.py ego 1 &
+python3 main.py ego 0 &
 cd ../ui/visualizer
 python3 visualizer.py ego &
 rosrun rviz rviz -d share_info.rviz
